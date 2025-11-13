b90f65d847c5eecc970832e558794be72bf6e8df
prashlesh <iamprashlesh@gmail.com>
2025-11-13 15:46:25 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 
 
-const app = require("express")
+const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -14,9 +14,10 @@ app.use(express.urlencoded({extended: true,limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import 
 
+import userRouter from './routes/user.routes.js'
 
-
-
-
+//routes declaration
+app.use("/api/v1/users",userRouter)
 export {app}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
     path:'./env'
 })
